ae7ccf9c0f4d09dd495f0e83050e7de5eaa01843
fatima.karaki <fatima.karaki@montyholding.com>
2024-05-23 15:46:05 +0300

    add fixes

--- src/main/java/com/example/demo/service/impl/SaleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SaleServiceImpl.java
@@ -4,16 +4,14 @@ import com.example.demo.common.OperationTypeEnum;
 import com.example.demo.common.Response;
 import com.example.demo.common.ResponseHelper;
 import com.example.demo.dto.SalesDto;
+import com.example.demo.dto.TransactionDto;
 import com.example.demo.dto.UpdateTransactionDto;
 import com.example.demo.entity.LogEntity;
 import com.example.demo.entity.SaleEntity;
 import com.example.demo.entity.SaleTransactionEntity;
 import com.example.demo.exception.BaseException;
 import com.example.demo.exception.ExceptionCode;
-import com.example.demo.repository.ClientRepository;
-import com.example.demo.repository.LogEntityRepository;
-import com.example.demo.repository.SaleRepository;
-import com.example.demo.repository.SaleTransactionRepository;
+import com.example.demo.repository.*;
 import com.example.demo.service.SaleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -26,6 +24,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -37,6 +36,7 @@ public class SaleServiceImpl implements SaleService
 	private final SaleRepository saleRepository;
 	private final SaleTransactionRepository saleTransactionRepository;
 	private final ClientRepository clientRepository;
+	private final ProductRepository productRepository;
 
 	@Override
 	public Response getSales(Integer limit, Integer offset)
@@ -54,7 +54,7 @@ public class SaleServiceImpl implements SaleService
 		{
 			throw new BaseException(ExceptionCode.SALE_NOT_FOUND);
 		}
-		return ResponseHelper.SUCCESS(saleRepository);
+		return ResponseHelper.SUCCESS(saleEntity);
 	}
 
 	@Override
@@ -67,18 +67,15 @@ public class SaleServiceImpl implements SaleService
 		}
 
 		SaleEntity saleEntity = new SaleEntity().setSeller(salesDto.getSeller())
-												.setTotal(salesDto.getTotal())
 												.setClientId(salesDto.getClientId())
 												.setCreationDate(LocalDateTime.now());
 
 		saleRepository.save(saleEntity);
 
-		salesDto.getTransactionDtoList().forEach(transactionDto -> {
-			SaleTransactionEntity saleTransactionEntity = new SaleTransactionEntity();
-			BeanUtils.copyProperties(transactionDto, saleTransactionEntity);
-			saleTransactionEntity.setSaleId(saleEntity.getId());
-			saleTransactionRepository.save(saleTransactionEntity);
-		});
+		double total = this.addTransactionsAndCalculatePrice(saleEntity.getId(), salesDto.getTransactionDtoList());
+
+		saleEntity.setTotal(total);
+		saleRepository.save(saleEntity);
 
 		return ResponseHelper.CREATED(saleEntity);
 	}
@@ -94,21 +91,27 @@ public class SaleServiceImpl implements SaleService
 		}
 		SaleTransactionEntity saleTransaction = saleTransactionEntity.get();
 
-		if(dto.getPrice() == null && dto.getQuantity() == null)
-		{
-			throw new BaseException(ExceptionCode.SALE_TRANSACTION_UPDATE_EMPTY);
-		}
-
 		OperationTypeEnum operationTypeEnum;
-		if(dto.getQuantity() != null)
+
+		if(dto.getQuantity() != null && dto.getPrice() != null)
+		{
+			saleTransaction.setQuantity(dto.getQuantity());
+			saleTransaction.setPrice(dto.getPrice());
+			operationTypeEnum = OperationTypeEnum.UPDATE_PRICE_AND_QUANTITY;
+		}
+		else if(dto.getQuantity() != null)
 		{
 			saleTransaction.setQuantity(dto.getQuantity());
 			operationTypeEnum = OperationTypeEnum.UPDATE_QUANTITY;
 		}
-		else
+		else if(dto.getPrice() != null)
 		{
 			saleTransaction.setPrice(dto.getPrice());
 			operationTypeEnum = OperationTypeEnum.UPDATE_PRICE;
+		}
+		else
+		{
+			throw new BaseException(ExceptionCode.SALE_TRANSACTION_UPDATE_EMPTY);
 		}
 
 		saleTransactionRepository.save(saleTransaction);
@@ -119,6 +122,40 @@ public class SaleServiceImpl implements SaleService
 
 		logEntityRepository.save(logEntity);
 		log.info("log of update added of operation {}", logEntity.getOperationType());
+
+		this.updateSaleTotal(transactionId);
 		return ResponseHelper.SUCCESS(saleTransaction);
+	}
+
+	private double addTransactionsAndCalculatePrice(Long saleId, List<TransactionDto> transactionDtoList)
+	{
+		double[] total = { 0.0 };
+		transactionDtoList.forEach(transactionDto -> {
+			SaleTransactionEntity saleTransactionEntity = new SaleTransactionEntity();
+			if(productRepository.findById(transactionDto.getProductId()).isEmpty())
+			{
+				throw new BaseException(ExceptionCode.PRODUCT_NOT_FOUND);
+			}
+			BeanUtils.copyProperties(transactionDto, saleTransactionEntity);
+			saleTransactionEntity.setSaleId(saleId);
+			saleTransactionRepository.save(saleTransactionEntity);
+			total[0] = total[0] + transactionDto.getPrice() * transactionDto.getQuantity();
+		});
+		return total[0];
+	}
+
+	private void updateSaleTotal(Long transactionId)
+	{
+		Optional<SaleTransactionEntity> saleTransactionEntity = saleTransactionRepository.findById(transactionId);
+		Optional<SaleEntity> saleEntity = saleRepository.findById(saleTransactionEntity.get().getSaleId());
+
+		SaleEntity sale = saleEntity.get();
+		double[] total = { 0.0 };
+		sale.getSaleTransactions().forEach(transaction -> {
+			total[0] = total[0] + transaction.getPrice() * transaction.getQuantity();
+		});
+
+		sale.setTotal(total[0]);
+		saleRepository.save(sale);
 	}
 }
